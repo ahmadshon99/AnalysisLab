5d5a56ac8c60437e53c7a7b553734dcabc293065
jannatOmar <jannat.omar2017@gmail.com>
2021-02-10 07:26:56 -0800

    comit 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,9 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
                     {{ __('You are logged in!') }}
                 </div>
+                
             </div>
         </div>
     </div>
